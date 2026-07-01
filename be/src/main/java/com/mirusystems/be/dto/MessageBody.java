80d6792b6c1b69528a0901700940beefe155ec51
settobae <sjbae@mirusystems.com>
2026-07-01 15:38:14 +0900

    feat: 기존 서버 삭제

--- be/src/main/java/com/mirusystems/be/dto/MessageBody.java
+++
@@ -1,9 +0,0 @@
-package com.mirusystems.be.dto;
-
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
-@JsonIgnoreProperties(ignoreUnknown = true)
-public class MessageBody {
-    public PositionReport PositionReport;
-    public ShipStaticData ShipStaticData;
-}
